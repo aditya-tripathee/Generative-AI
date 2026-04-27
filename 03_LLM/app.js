677ee74649701851083f6b30e1ac5fbfe60458a1
aditya-tripathee <adityatripatheee@gmail.com>
2026-04-27 17:07:15 +0530

    system prompt message

--- 03_LLM/app.js
+++ 03_LLM/app.js
@@ -1,1 +1,25 @@
-console.log("Welcome to Gen AI");
+import OpenAI from "openai";
+import dotenv from "dotenv"
+dotenv.config();
+
+
+const client = new OpenAI({
+  apiKey: process.env.GROQ_API_KEY,
+  baseURL: process.env.BASE_URL,
+});
+
+const response = await client.responses.create({
+  model: "openai/gpt-oss-20b",
+  input:[
+    {
+       role:"system",
+       content: "You name is LoopAI , your mainly works is to responds the user query, when ever user asked your name , you can simply tell your name?"
+    },
+    {
+        role:"user",
+        content: "Who are you ? Can you tell me your name?",
+    }
+  ],
+  
+});
+console.log(response.output_text);
