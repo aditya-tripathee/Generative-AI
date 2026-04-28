ed8c121423c3cb445bc21318cfa688644116708d
aditya-tripathee <adityatripatheee@gmail.com>
2026-04-28 18:11:57 +0530

    llm

--- 03_LLM/app.js
+++ 03_LLM/app.js
@@ -1,5 +1,5 @@
 import OpenAI from "openai";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 dotenv.config();
 
 const client = new OpenAI({
@@ -9,18 +9,17 @@ const client = new OpenAI({
 
 const response = await client.responses.create({
   model: "openai/gpt-oss-20b",
-  temperature:1, // temperature shoule be between from 0 to 2 and middle one is 1 which gives relatistics values and output 
-  
-  input:[
+  temperature: 1, // temperature shoule be between from 0 to 2 and middle one is 1 which gives relatistics values and output
+  input: [
     {
-       role:"system",
-       content: "You name is LoopAI , your mainly works is to responds the user query, when ever user asked your name , you can simply tell your name?"
+      role: "system",
+      content:
+        "You name is LoopAI , your mainly works is to responds the user query, when ever user asked your name , you can simply tell your name?",
     },
     {
-        role:"user",
-        content: "Who are you ? Can you tell me your name? What is llm models ?",
-    }
+      role: "user",
+      content: "Who are you ? Can you tell me your name? What is llm models ?",
+    },
   ],
-  
 });
 console.log(response.output_text);
